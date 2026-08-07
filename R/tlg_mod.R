50d28001e80d661e31d22a99ae481c11824d938a
Ashley Baldry <arbaldry91@gmail.com>
2026-08-07 12:04:58 +0100

    Docs update

--- R/tlg_mod.R
+++ R/tlg_mod.R
@@ -53,11 +53,18 @@ tlgModUI <- function(id, params = NULL) {
   )
 }
 
+#' @param dtlg_fn Function body of the function to use from the dtlg pcakage
+#' @param other_fn Function body of the function to use as a comparison output
+#' @param n_iter (Reactive) Number of iterations to run the comparison over
+#' @param dataset List of datasets to use as part of the comparison. Must be named
+#' in the same way that they are arguments in `dtlg_fn` and `other_fn`
+#' @param filter_params A named list of arguments in `dtlg_fn` that can be updated
+#' in the UI for different tables
+#' @param other_params A named list of additional arguments passed to `dtlg_fn`
+#'
 #' @rdname tlg_mod
 tlgModServer <- function(id, dtlg_fn, other_fn, n_iter, datasets, filter_params, other_params) {
   moduleServer(id, function(input, output, session) {
-    ns <- session$ns
-
     #### Parameters ####
     dataset_names <- reactive(purrr::map(datasets, names))
 
